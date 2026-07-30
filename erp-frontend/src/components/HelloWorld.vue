746f2bebdcf9e64e397bc3aa876d619fe3f8b455
VdYisus <darkknightgamer76@gmail.com>
2026-07-30 10:21:52 -0400

    agregando nombres

--- erp-frontend/src/components/HelloWorld.vue
+++ erp-frontend/src/components/HelloWorld.vue
@@ -12,8 +12,12 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Proyecto ERP Grupo #7
         </h1>
+        <p>Realizado por: </p>
+        <p>Jose Correa</p>
+        <p>Jonathan Campos</p>
+        <p>Jesús Dorante</p>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
